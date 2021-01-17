e7deca65e1d39a60d85eafb89820a6a90708d9e5
Zaidomar0555 <zaidomar0555@gmail.com>
2021-01-17 10:28:52 +0300

    done a from for register and login: check register/login controllers and view pages as well

---
+++ posty/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
+class RegisterController extends Controller
+{
+    public function index(){
+        return view('auth.register');
+    }
+
+
+    public function store(Request $request){
+        /*
+        What i want from this function is to: 
+
+        1- validation
+        2- store user
+        3- sign the user in
+        4- redirect the user
+         */
+
+         // 1
+
+         $this->validate($request, [
+             'name' => 'required|max:255',
+             'username' => 'required',
+             'email' => 'required|email|max:255',
+             'password' => 'required|confirmed'
+
+         ]);
+        
+         //2
+
+         User::create([
+             'name' => $request->name,
+             'username' => $request->username,
+             'email' => $request->email,
+             'password' => Hash::make($request->password)
+         ]);
+
+         // 3
+
+         auth()->attempt($request->only('email', 'password'));
+
+         // 4
+
+         return redirect()->route('dashboard');
+         
+
+
+    }
+}
--- posty/resources/views/layouts/app.blade.php
+++ posty/resources/views/layouts/app.blade.php
@@ -17,10 +17,17 @@
         </ul>
 
         <ul class="flex items-center">
+            @auth
             <li><a href="" class="p-3">Zaid Alsuwayyigh</a></li>
-            <li><a href="" class="p-3">Login</a></li>
-            <li><a href="" class="p-3">Register</a></li>
             <li><a href="" class="p-3">Logout</a></li>
+            @endauth
+
+            @guest
+            <li><a href="{{ route('login') }}" class="p-3">Login</a></li>
+            <li><a href="{{ route('register') }}" class="p-3">Register</a></li>
+            @endguest
+           
+            
         </ul>
     </nav>
     @yield('content')
