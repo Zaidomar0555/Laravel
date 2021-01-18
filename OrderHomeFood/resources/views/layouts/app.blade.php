ee1dc49dae5125699d8b11548c3c1ce4092ee719
Zaidomar0555 <zaidomar0555@gmail.com>
2021-01-18 12:55:25 +0300

    designed navbar and still working on home page to add more details and make it look nicer

---
+++ OrderHomeFood/resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title> Traditional Home Food</title>
+</head>
+<body class="bg-white">
+<nav class="p-6 bg-gray-200 flex justify-between mb-6">
+        <ul class="flex items-center">
+            <li><a href="/home" class="p-3">Home</a></li>
+            <li><a href="" class="p-3">Menu</a></li>
+            <li><a href="" class="p-3">Post</a></li>
+        </ul>
+
+        <ul class="flex items-center">
+         
+            <li>
+                 <a href="" class="p-3">Zaid</a>
+            </li>
+            <li>
+              <a href="" class="p-3">Logout</a>
+            </li>
+            <li><a href="" class="p-3">Login</a></li>
+            <li><a href="" class="p-3">Register</a></li>
+            
+        </ul>
+    </nav>
+    @yield('content')
+</body>
+</html>
